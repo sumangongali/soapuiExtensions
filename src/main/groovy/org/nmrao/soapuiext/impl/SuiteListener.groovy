6fb3ab6631e5e8de6ce9350f90d9bdae8af5ebd6
sumangongali <gsuman2009@gmail.com>
2026-01-06 13:36:58 -0500

    Replace log4j with log4j2 in SuiteListener

--- src/main/groovy/org/nmrao/soapuiext/impl/SuiteListener.groovy
+++ src/main/groovy/org/nmrao/soapuiext/impl/SuiteListener.groovy
@@ -1,14 +1,14 @@
 package org.nmrao.soapuiext.impl
 import com.eviware.soapui.model.support.TestSuiteListenerAdapter
 import com.eviware.soapui.model.testsuite.TestStep
-import org.apache.log4j.Logger
+import org.apache.logging.log4j.Logger
 
-import static org.apache.log4j.Logger.getLogger
+import org.apache.logging.log4j.LogManager;
 import static org.nmrao.soapuiext.SoapUIGroovyScriptEngineHelper.getInstance
 
 class SuiteListener extends TestSuiteListenerAdapter {
 
-    private final static Logger log = getLogger(SuiteListener)
+     private final static Logger log = LogManager.getLogger(SuiteListener)
     @Override
     void testStepAdded(TestStep testStep, int index) {
         def scriptEngineHelper = getInstance('TEST_SUITE_TEST_STEP_ADDED',null,null,log)
